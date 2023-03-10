ececca4678f8a381c99e2f6213bfdf680d417171
Ayush kedia <kediaayush26@gmail.com>
2023-03-10 13:53:08 +0530

    removed errors

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -21,12 +21,12 @@ const handlehover = (e) => {
   if (e.target.classList.contains("nav__link")) {
     const link = e.target;
     const siblings = link.closest(".nav").querySelectorAll(".nav__link");
-    const logo = link.closest(".nav").querySelector("img");
+
 
     siblings.forEach((el) => {
       if (el !== link) el.style.opacity = 0.5;
     });
-    logo.style.opacity = 0.5;
+  
   }
 };
 
@@ -35,12 +35,12 @@ const handleout = (e) => {
     // alert("dd")
     const link = e.target;
     const siblings = link.closest(".nav").querySelectorAll(".nav__link");
-    const logo = link.closest(".nav").querySelector("img");
+
 
     siblings.forEach((el) => {
       if (el !== link) el.style.opacity = 1;
     });
-    logo.style.opacity = 1;
+   
   }
 };
 
